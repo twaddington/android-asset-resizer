6dd870227de7262780a26899ca5c00ec68dbc4d1
Tristan Waddington <tristan.waddington@gmail.com>
2013-04-27 19:35:06 -0700

    Add unbuffered script output; stub out resizer.

--- android_asset_resizer/resizer.py
+++ android_asset_resizer/resizer.py
@@ -21,35 +21,28 @@ def mkres(path):
     Creates a directory tree for the resized Android assets.
     """
     for d in DENSITY_TYPES:
-        os.makedirs('%s/res/drawable-%s' % (os.path.abspath(path), d), 0755)
+        try:
+            os.makedirs('%s/res/drawable-%s' % (os.path.abspath(path), d), 0755)
+        except OSError:
+            pass
 
 def resize(path, out, density='xhdpi', prefix='', filter=Image.ANTIALIAS):
-    print 'resizing!'
-    return
-
     """
-    for path in args.pattern:
-        # TODO: Verify this is a file
-        if os.path.isfile(path):
-            print path
-            try:
-                im = Image.open(path)
-                
-                w, h = im.size
-
-                # TODO: Function for calculating out width and height
-                size = (int(w * .75), int(h * .75))
-                print im.size
-                print size
-
-                _, filename = os.path.split(path)
-
-                # Try other options, provide setting? Try BILINEAR?
-                # TODO: Make this a method!
-                # TODO: drawable-xhdpi dir!
-                # TODO: Replace '@2x'
-                # TODO: Add prefix!
-                im.resize(size, Image.ANTIALIAS).save(os.path.join(out, filename))
-            except IOError:
-                pass
     """
+    im = Image.open(path) 
+
+    w, h = im.size
+
+    # TODO: Function for calculating out width and height
+    size = (int(w * .75), int(h * .75))
+    print im.size
+    print size
+
+    _, filename = os.path.split(path)
+
+    # Try other options, provide setting? Try BILINEAR?
+    # TODO: Make this a method!
+    # TODO: drawable-xhdpi dir!
+    # TODO: Replace '@2x'
+    # TODO: Add prefix!
+    #im.resize(size, Image.ANTIALIAS).save(os.path.join(out, filename))
