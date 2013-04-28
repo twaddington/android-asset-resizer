8a2fd72102f8bd65fdafb82dcdd5cace554be78f
Tristan Waddington <tristan.waddington@gmail.com>
2013-04-27 17:51:50 -0700

    Move version to module.

---
+++ android_asset_resizer/__init__.py
@@ -0,0 +1,12 @@
+"""
+android-asset-resizer
+
+:copyright: (c) 2013 by Tristan Waddington.
+:license: Apache 2.0, see LICENSE for more details.
+
+"""
+
+__version__ = '1.0.0'
+__author__ = 'Tristan Waddington'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2013 Tristan Waddington'
