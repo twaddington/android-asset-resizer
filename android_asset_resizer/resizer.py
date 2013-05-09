8ac57787434eeb370e5574663d9b9a4dcade4916
Tristan Waddington <tristan.waddington@gmail.com>
2013-05-08 23:35:24 -0700

    Clean up source; add error checking and docstrings.

--- android_asset_resizer/resizer.py
+++ android_asset_resizer/resizer.py
@@ -1,58 +1,5 @@
-"""
-"""
-
 import os
-
 from PIL import Image
-
-"""
-3:4:6:8:12
-
-xhdpi * .375 = ldpi
-xhdpi * .50  = mdpi
-xhdpi * .75  = hdpi
-xhdpi * 1.0  = xhdpi
-xhdpi * 1.5  = xxhdpi
-
-    3 / 8   = ldpi
-    4 / 8   = mdpi
-    6 / 8   = hdpi
-    8 / 8   = xhdpi
-    12 / 8  = xxhdpi
-
-xxhdpi / .375 = ldpi
-xxhdpi / .50  = mdpi
-xxhdpi / .75  = hdpi
-xxhdpi / 1.5  = xhdpi
-xxhdpi / 1    = xxhdpi
-
-    3 / 12   = ldpi
-    4 / 12   = mdpi
-    6 / 12   = hdpi
-    8 / 12   = xhdpi
-    12 / 12  = xxhdpi
-
-current_size * (target_density / current_density) = target_size
-
-24w * (3 / 6)   = 12w # hdpi to ldpi
-24w * (4 / 6)   = 16w # hdpi to mdpi
-24w * (6 / 6)   = 24w # hdpi to hdpi
-24w * (8 / 6)   = 32w # hdpi to xhdpi
-24w * (12 / 6)  = 48w # hdpi to xxhdpi
-
-24w * (3 / 8)   = 9w  # xhdpi to ldpi
-24w * (4 / 8)   = 12w # xhdpi to mdpi
-24w * (6 / 8)   = 18w # xhdpi to hdpi
-24w * (8 / 8)   = 24w # xhdpi to xhdpi
-24w * (12 / 8)  = 36w # xhdpi to xxhdpi
-
-24w * (3 / 12)  = 6w  # xxhdpi to ldpi
-24w * (4 / 12)  = 8w  # xxhdpi to mdpi
-24w * (6 / 12)  = 12w # xxhdpi to hdpi
-24w * (8 / 12)  = 16w # xxhdpi to xhdpi
-24w * (12 / 12) = 24w # xxhdpi to xxhdpi
-
-"""
 
 DENSITY_TYPES = ('ldpi', 'mdpi', 'hdpi', 'xhdpi', 'xxhdpi')
 DENSITY_MAP = {
@@ -64,10 +11,11 @@ DENSITY_MAP = {
 }
 
 class AssetResizer():
-    """
-    """
     def __init__(self, out, source_density='xhdpi', prefix='',
             filter=Image.ANTIALIAS):
+        if source_density not in DENSITY_TYPES:
+            raise ValueError('source_density must be one of %s' % str(DENSITY_TYPES))
+
         self.out = os.path.abspath(out)
         self.source_density = source_density
         self.prefix = prefix
@@ -75,7 +23,7 @@ class AssetResizer():
 
     def mkres(self):
         """
-        Creates a directory tree for the resized Android assets.
+        Create a directory tree for the resized assets
         """
         for d in DENSITY_TYPES:
             try:
@@ -86,45 +34,50 @@ class AssetResizer():
 
     def get_out_for_density(self, target_density):
         """
-        Return the out directory for the given target density.
+        Return the out directory for the given target density
         """
         return os.path.join(self.out, 'res/drawable-%s' % target_density)
 
     def get_size_for_density(self, size, target_density):
         """
-        current_size * (target_density / current_density) = target_size
+        Return the new image size for the target density
         """
         current_size = size
         current_density = DENSITY_MAP[self.source_density]
         target_density = DENSITY_MAP[target_density]
 
-        #print "%s * (%s / %s)" % (current_size, target_density, current_density)
         return int(current_size * (target_density / current_density))
+
+    def get_safe_filename(self, filename):
+        """
+        Return a sanitized image filename
+        """
+        return filename.replace('@2x', '').replace('-', '_')
 
     def resize(self, path):
         """
+        Generate assets from the given image
         """
         im = Image.open(path) 
 
-        # ...
+        # Get the original filename
         _, filename = os.path.split(path)
 
-        # ...
-        filename = '%s%s' % (self.prefix if self.prefix else '', filename.replace('@2x', ''))
+        # Generate the new filename
+        filename = self.get_safe_filename(filename)
+        filename = '%s%s' % (self.prefix if self.prefix else '', filename)
 
-        # ...
+        # Get the original image size
         w, h = im.size
 
-        # ...
+        # Generate assets from the source image
         for d in DENSITY_TYPES:
-            # TODO: Don't resize if source_density == d, just move and rename
-            size = (self.get_size_for_density(w, d), self.get_size_for_density(h, d))
+            out_file = os.path.join(self.out,
+                    self.get_out_for_density(d), filename)
 
-            #print '%s => %s: %s => %s' % (self.source_density, d, im.size, size)
-
-            # ...
-            out_file = os.path.join(self.out, self.get_out_for_density(d), filename)
-            #print 'Saving to %s' % out_file
-
-            # ...
-            im.resize(size, Image.ANTIALIAS).save(out_file)
+            if d == self.source_density:
+                im.save(out_file)
+            else:
+                size = (self.get_size_for_density(w, d),
+                        self.get_size_for_density(h, d))
+                im.resize(size, self.filter).save(out_file)
