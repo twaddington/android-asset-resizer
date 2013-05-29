5ed3e1b8cb30d23f46eec33a689d40895d5848a2
Tristan Waddington <tristan.waddington@gmail.com>
2013-05-29 01:34:52 -0700

    Bump version.

--- android_asset_resizer/__init__.py
+++ android_asset_resizer/__init__.py
@@ -6,7 +6,7 @@ android-asset-resizer
 
 """
 
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 __author__ = 'Tristan Waddington'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2013 Tristan Waddington'
